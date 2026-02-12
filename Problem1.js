e63bbe309026cb0df12349078c30fffcd1019ca5
ganeshpenta <gpenta310@gmail.com>
2026-02-12 00:29:24 -0500

    This is trial version

---
+++ Problem1.js
@@ -0,0 +1,9 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2867
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 let a = 3;\
+(a%2==0) ? console.log(\'93a is even\'94) : console.log(\'93a is odd\'94);}
